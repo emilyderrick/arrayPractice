27d252fe5e27c68e1bb7e4f7c250984932363415
DERRICK <derricke@otc.edu>
2018-10-30 09:39:01 -0500

    added new examples

--- firstProject.py
+++ firstProject.py
@@ -1,1 +1,12 @@
-print("hello world")
+import random
+aryList = ["cookies","candy","coffee"]
+
+for i in range(3):
+    choice = input("What else shall I add?")
+    aryList.append(choice)
+
+#print(aryList)
+for i in range(6):
+    print(aryList[i])
+number = random.randint(0,5)
+print(aryList[number])
---
+++ new.py
@@ -0,0 +1,17 @@
+# tshirt = ["small","medium","large"]
+# price = [8,9,12]
+
+# for i in range(len(tshirt)):
+#     print(tshirt[i], price[i])
+# tshirts = [["small",8],["medium",9],["large",12]]
+
+# for i in range(len(tshirts)):
+#     print(tshirts[i][0],tshirts[i][1])
+
+
+#string formatting
+name = "Fido"
+age = 10
+#print(name + " is " + str(age) + " years old.")
+#print("{} is {} years old".format(name,age))
+print(f"{name} is {age} years old")
